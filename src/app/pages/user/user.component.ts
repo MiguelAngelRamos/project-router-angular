6b41e7af2c2f5f22ec0ecd8fa4ac0214e966147b
Miguel Ramos <return.infox@gmail.com>
2024-08-29 11:24:05 -0400

    parametro

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  // http:localhost:4200/user/6
+  constructor(private dataService: DataService, private activatedRoute: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.getUser(); // invoca al método getUser()
+  }
+
+  getUser() {
+    this.activatedRoute.paramMap.subscribe(console.log)
+  }
 }
