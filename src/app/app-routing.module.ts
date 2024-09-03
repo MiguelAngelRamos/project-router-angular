4bb633091d9d98675f1ea9d496b083a83efe8889
Miguel Ramos <return.infox@gmail.com>
2024-09-03 11:17:21 -0400

    error 404 terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { UserComponent } from './pages/user/user.component';
 import { UsersComponent } from './pages/users/users.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 // http://localhost:4200/
 // http://localhost:4200/user/:id
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'user/:id', component: UserComponent},
   { path: 'users', component: UsersComponent},
-  { path: '**', redirectTo: 'home', pathMatch: 'full'}
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: Error404Component}
 ];
 
 @NgModule({
